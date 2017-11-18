79c0d276e2e6702312ddc509ff762d0ba46f8bfa
yhuang <yhuang@trustwave.com>
2017-11-18 08:59:40 -0500

    add source code comment

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,19 @@
+/*  TLVServer
+ *  ===============
+ *  Copyright (C) 2017 yaohuang2005@gmail.com
+ *
+ *  Licensed under the GNU LESSER GENERAL PUBLIC LICENSE
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 #include <iostream>
 #include <boost/asio.hpp>
 #include "server_tcpsrv.h"
@@ -5,8 +21,6 @@
 
 #define LOCAL_ADDR  "127.0.0.1"
 #define SERV_PORT   1234
-
-
 
 int main(int argc,char* argv[])
 {
