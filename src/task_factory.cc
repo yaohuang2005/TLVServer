68baae78d941f7c5edc3b6b68ff4989a0af5cfff
yhuang <yhuang@trustwave.com>
2017-11-18 09:06:05 -0500

    change comment

--- src/task_factory.cc
+++ src/task_factory.cc
@@ -3,11 +3,6 @@
  *  Copyright (C) 2017 yaohuang2005@gmail.com
  *
  *  Licensed under the GNU LESSER GENERAL PUBLIC LICENSE;
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
