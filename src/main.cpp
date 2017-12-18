08fb1f76c8a75015441da2049d1fe7a35e05b484
yhuang <yhuang@trustwave.com>
2017-12-18 06:39:23 -0500

    remove one line

--- src/main.cpp
+++ src/main.cpp
@@ -120,7 +120,6 @@ int main(int argc,char* argv[])
 
     ThreadPool *threadPool = new ThreadPool(number);
 
-  //  TcpServer server(threadPool);
     server = new TcpServer(threadPool);
     /* Signal handler */
     signalInit();
